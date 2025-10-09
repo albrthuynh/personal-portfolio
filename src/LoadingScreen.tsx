f83604e0b41df7f687794a94cac229f1b46008fc
albrthuynh <ahuyn2@uic.edu>
2025-10-09 14:35:50 -0500

    lowered the loading time

--- src/LoadingScreen.tsx
+++ src/LoadingScreen.tsx
@@ -11,7 +11,7 @@ export default function LoadingScreen({ onLoadingComplete }: LoadingScreenProps)
             initial={{ opacity: 1 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.5, delay: 1.8 }}
+            transition={{ duration: 0.5, delay: 1 }}
             onAnimationComplete={onLoadingComplete}
         >
             {/* Animated background gradients */}
